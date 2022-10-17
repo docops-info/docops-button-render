4f3c14d7cd0919096fec08b3e92e5754698ccf4a
Steve Roach <sroachst@gmail.com>
2022-10-17 19:43:47 -0400

    Removed bullets from list of links, used xlink:href for proper svg links target self or new window when link is clicked.

--- src/main/kotlin/io/docops/asciidoc/buttons/RectangleCardRenderer.kt
+++ src/main/kotlin/io/docops/asciidoc/buttons/RectangleCardRenderer.kt
@@ -40,15 +40,6 @@ class RectangleCardRenderer : ButtonMaker() {
     private fun makeStyles(buttons: MutableList<MutableList<Button>>, theme: Theme): String {
         //language=html
         return """<style>
-        div {
-            color: #000000;
-            font: 15px "Inter var", system-ui, "Helvetica Neue", Helvetica, Arial, sans-serif;
-            height: 100%;
-            overflow: auto;
-        }
-        li {
-            text-underline: none;
-        }
         rect {
             stroke: #d2ddec;
         }
@@ -61,6 +52,14 @@ class RectangleCardRenderer : ButtonMaker() {
             cursor: pointer;
         }
 
+        .linkText {
+            fill: #265301;;
+            font-size: 15px;
+            font-family: "Inter var", system-ui, "Helvetica Neue", Helvetica, Arial, sans-serif;
+            font-weight: normal;
+            cursor: pointer;
+            text-decoration: underline;
+        }
 
         .shadow {
             -webkit-filter: drop-shadow(3px 3px 2px rgba(0, 0, 0, .7));
@@ -70,6 +69,10 @@ class RectangleCardRenderer : ButtonMaker() {
     }
 
     private fun createItem(button: Button, row: Int, column: Int, theme: Theme): String {
+        var window = "_top"
+        if(theme.newWin) {
+            window = "_blank"
+        }
         val rowHeight = 120
         val itemHeight = (rowHeight * (row)) + 10 + (row * spacer)
         val width = 293
@@ -77,28 +80,25 @@ class RectangleCardRenderer : ButtonMaker() {
         //language=svg
         return """
         <g>
-        
         <rect x="$itemWidth" y="$itemHeight" width="$width" height="$rowHeight" rx="12" ry="12" fill="#ffffff" fill-opacity='0.3;'/>
         <text x="${itemWidth+15+105}" y="${itemHeight+25}" class="titleText">${button.title.escapeXml()}</text>
         <rect x="${itemWidth+10}" y="${itemHeight+10}" height="98" width="98" rx="12" ry="12" fill="${theme.buttonColor(button)}"/>
         ${makeButtonImage(button, x = itemWidth+12,y = itemHeight+15)}
-        <foreignObject x="${itemWidth + 15 + 105}" y="${itemHeight + 30}" width="180" height="200">
-                <div xmlns="http://www.w3.org/1999/xhtml">${linksToList(button)}</div>
-        </foreignObject>
-    </g>
+        ${makeLinks(itemWidth+15+105, itemHeight + 30, button, window)}
+        </g>
         """.trimIndent()
     }
-//${button.description.escapeXml().take(80)}
-    private fun linksToList(button: Button): String {
+
+
+    private fun makeLinks(x: Int, y: Int, button: Button, window: String): String {
         val sb = StringBuilder()
+        sb.append("""<text x="$x" y="$y" class="linkText">""".trimIndent())
         button.links?.forEach {
-            sb.append("""<li>
-                <a href="${it.href}">${it.label}</a>
-                </li>""".trimMargin())
+            sb.append("""<tspan x="$x" dy="16"><a xlink:href="${it.href}" target="$window">${it.label}</a></tspan>""")
         }
+        sb.append("</text>")
         return sb.toString()
     }
-
     private fun makeButtonImage(button: Button, x: Int, y: Int): String {
         button.buttonImage?.let {
             return """
@@ -109,32 +109,6 @@ class RectangleCardRenderer : ButtonMaker() {
     }
 
     private fun createDefs(buttons: MutableList<MutableList<Button>>, theme: Theme): String {
-        return """<defs>
-    <filter id="duotone" x="-10%" y="-10%" width="120%" height="120%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feColorMatrix type="matrix" values="1 0 0 0 0
-1 0 0 0 0
-1 0 0 0 0
-0 0 0 1 0" in="SourceGraphic" result="colormatrix"/>
-        <feComponentTransfer in="colormatrix" result="componentTransfer">
-            <feFuncR type="table" tableValues="0.27 0.86 0.97"/>
-            <feFuncG type="table" tableValues="0.01 0.08 0.81"/>
-            <feFuncB type="table" tableValues="0.02 0.24 0.05"/>
-            <feFuncA type="table" tableValues="0 1"/>
-        </feComponentTransfer>
-    </filter>
-        <filter id="redsunset" x="-10%" y="-10%" width="120%" height="120%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-            <feColorMatrix type="matrix" values="1 0 0 0 0
-1 0 0 0 0
-1 0 0 0 0
-0 0 0 1 0" in="SourceGraphic" result="colormatrix"/>
-            <feComponentTransfer in="colormatrix" result="componentTransfer">
-                <feFuncR type="table" tableValues="0.43 0.97"/>
-                <feFuncG type="table" tableValues="0.06 0.88"/>
-                <feFuncB type="table" tableValues="0.37 0.79"/>
-                <feFuncA type="table" tableValues="0 1"/>
-            </feComponentTransfer>
-            <feBlend mode="screen" in="componentTransfer" in2="SourceGraphic" result="blend"/>
-        </filter>
-    </defs>"""
+        return """<defs/>"""
     }
 }
